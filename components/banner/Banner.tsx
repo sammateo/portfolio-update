b6de5463b80760da934edbf949274dd235fde2d1
Mateo Sam <sammateo4@gmail.com>
2024-04-26 23:06:22 -0400

    resize banner

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Banner() {
 	return (
 		<section className=" bg-primary text-primary-foreground">
-			<div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+			<div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-[89.7vh] lg:items-center">
 				<div className="mx-auto max-w-3xl text-center">
 					<h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
 						Mateo Sam
