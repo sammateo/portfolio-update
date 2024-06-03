1dc25ae0f0bb4344176ac3cfed52dd01f92d4b98
Mateo Sam <sammateo4@gmail.com>
2024-06-02 21:55:45 -0400

    projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,30 @@
 import ComingSoon from "@/components/Misc/ComingSoon";
+import Navigation from "@/components/navigation/Navigation";
 import React from "react";
-
+import projectList from "@/data/projects";
+import ProjectRow from "@/components/projects/ProjectRow";
 export default function page() {
 	return (
 		<div>
-			<ComingSoon />
+			<Navigation />
+			<div className=" md:mt-20">
+				<h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-center w-fit mx-0 md:mx-auto text-3xl px-4 font-extrabold text-transparent sm:text-5xl">
+					Projects
+				</h1>
+				<div className="[&>*:nth-child(even)]:bg-[#12355B]">
+					{projectList.map((project) => (
+						<ProjectRow
+							key={project.id}
+							id={project.id}
+							title={project.title}
+							description={project.description}
+							image={project.image}
+							url={project.url}
+							github={project.github}
+						/>
+					))}
+				</div>
+			</div>
 		</div>
 	);
 }
--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -25,7 +25,7 @@ export default function Navigation() {
 	];
 	return (
 		<div>
-			<div className="hidden md:flex bg-gray-900 text-primary-foreground justify-center gap-x-5 py-4 ">
+			<div className="hidden md:flex bg-gray-900 bg-transparent text-primary-foreground justify-center gap-x-5 py-4 z-10 relative">
 				{navitems.map((item) => (
 					<Link
 						key={item.name}
@@ -50,6 +50,19 @@ export default function Navigation() {
 				setOpenNav={setOpenNav}
 				navitems={navitems}
 			/>
+			{/* <svg
+				className="absolute top-0 -left-30 sm:left-0 z-0"
+				width="304"
+				height="210"
+				viewBox="0 0 304 210"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					d="M157.475 -10H220.939C271.024 -10 309.583 34.2192 302.765 83.8384C296.351 130.518 252.248 162.383 205.916 153.812L105.935 135.319C65.4378 127.828 24.166 143.718 -0.851048 176.433L-14.9286 194.842C-16.0747 196.341 -16.8529 198.088 -17.2007 199.942C-19.7982 213.796 -39.9911 212.693 -41.0645 198.639L-54.1514 27.296C-55.6883 7.17419 -39.7758 -10 -19.5953 -10H157.475Z"
+					fill="#12355B"
+				/>
+			</svg> */}
 		</div>
 	);
 }
